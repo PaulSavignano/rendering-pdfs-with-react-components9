0c1b23619f289794b0c10bdd64509785279701b9
Paul Savignano <Paul.Savignano@gmail.com>
2016-08-31 09:23:26 -0700

    Added AddDocument component.

--- imports/ui/components/add-document.js
+++ imports/ui/components/add-document.js
@@ -1,32 +1,46 @@
-import React from 'react';
-import { FormGroup, FormControl } from 'react-bootstrap';
-import { Bert } from 'meteor/themeteorchef:bert';
-import { insertDocument } from '../../api/documents/methods.js';
+import React from 'react'
+import { FormGroup, FormControl, Button } from 'react-bootstrap'
+import { Bert } from 'meteor/themeteorchef:bert'
+import { insertDocument } from '../../api/documents/methods'
 
 const handleInsertDocument = (event) => {
-  const target = event.target;
-  const title = target.value.trim();
-
-  if (title !== '' && event.keyCode === 13) {
+  event.preventDefault()
+  const title = document.querySelector('[name="title"]')
+  const body = document.querySelector('[name="body"]')
+  if (title.value.trim() !== '' && body.value.trim() !== '') {
     insertDocument.call({
-      title,
+      title: title.value,
+      body: body.value,
     }, (error) => {
       if (error) {
-        Bert.alert(error.reason, 'danger');
+        Bert.alert(error.reason, 'danger')
       } else {
-        target.value = '';
-        Bert.alert('Document added!', 'success');
+        title.value = ''
+        body.value = ''
+        Bert.alert('Document Added!', 'success')
       }
-    });
+    })
+  } else {
+    Bert.alert('Both a title and body are required.', 'danger')
   }
-};
+}
 
 export const AddDocument = () => (
-  <FormGroup>
-    <FormControl
-      type="text"
-      onKeyUp={ handleInsertDocument }
-      placeholder="Type a document title and press enter..."
-    />
-  </FormGroup>
-);
+  <form onSubmit={ handleInsertDocument } className="AddDocument">
+    <FormGroup>
+      <FormControl
+        name="title"
+        type="text"
+        placeholder="Type a document title."
+      />
+    </FormGroup>
+    <FormGroup>
+      <FormControl
+        name="body"
+        componentClass="textarea"
+        placeholder="Type a document body."
+      />
+    </FormGroup>
+    <Button type="submit" bsStyle="success">Add Document</Button>
+  </form>
+)
