865059f2b2c580c1a7528a66f3c0c038fa595412
Paul Savignano <Paul.Savignano@gmail.com>
2016-08-31 13:42:39 -0700

    Added downloadDocument method.

---
+++ imports/api/documents/server/methods.js
@@ -0,0 +1,29 @@
+import { Meteor } from 'meteor/meteor'
+import { SimpleSchema } from 'meteor/aleed:simple-schema'
+import { ValidatedMethod } from 'meteor/mdg:valiated-method'
+import { Documents } from '../documents'
+import { Document } from '../../../ui/components/document'
+import { generateComponentAsPDF } from '../../../modules/server/generate-pdf'
+import { rateLimit } from '../../../modules/rate-limit'
+
+export const downloadDocument = new ValidatedMethod({
+  name: 'documents.download',
+  validate: new SimpleSchema({
+    documentId: { type: String },
+  }).validator(),
+  run({ documentId }) {
+    const document = Documents.findOne({ _id: documentId })
+    const fileName = `document_${document._id}.pdf`
+    return generateComponentAsPDF({ component: Document, props: { document }, fileName })
+    .then((result) => result)
+    .catch((error) => { throw new Meteor.Error('500', error) })
+  },
+})
+
+rateLimit({
+  methods: [
+    downloadDocument,
+  ],
+  limit: 1,
+  timeRange: 1000,
+})
--- imports/ui/components/document.js
+++ imports/ui/components/document.js
@@ -1,60 +1,48 @@
-import React from 'react';
-import { Row, Col, ListGroupItem, FormControl, Button } from 'react-bootstrap';
-import { Bert } from 'meteor/themeteorchef:bert';
-import { updateDocument, removeDocument } from '../../api/documents/methods.js';
+import React from 'react'
+import InlineCss from 'react-inline-css'
+import { Button, ListGroupItem } from 'react-bootstrap'
+import { Meteor } from 'meteor/meteor'
+import { Bert } from 'meteor/themeteorchef:bert'
 
-const handleUpdateDocument = (documentId, event) => {
-  const title = event.target.value.trim();
-  if (title !== '' && event.keyCode === 13) {
-    updateDocument.call({
-      _id: documentId,
-      update: { title },
-    }, (error) => {
-      if (error) {
-        Bert.alert(error.reason, 'danger');
-      } else {
-        Bert.alert('Document updated!', 'success');
-      }
-    });
-  }
-};
+const handleDownloadDocument = (event) => {
+  event.preventDefault()
+}
 
-const handleRemoveDocument = (documentId, event) => {
-  event.preventDefault();
-  // this should be replaced with a styled solution so for now we will
-  // disable the eslint `no-alert`
-  // eslint-disable-next-line no-alert
-  if (confirm('Are you sure? This is permanent.')) {
-    removeDocument.call({
-      _id: documentId,
-    }, (error) => {
-      if (error) {
-        Bert.alert(error.reason, 'danger');
-      } else {
-        Bert.alert('Document removed!', 'success');
-      }
-    });
-  }
-};
+const handleRemoveDocument = (event) => {
+  event.preventDefault()
+}
 
 export const Document = ({ document }) => (
-  <ListGroupItem key={ document._id }>
-    <Row>
-      <Col xs={ 8 } sm={ 10 }>
-        <FormControl
-          type="text"
-          defaultValue={ document.title }
-          onKeyUp={ handleUpdateDocument.bind(this, document._id) }
-        />
-      </Col>
-      <Col xs={ 4 } sm={ 2 }>
-        <Button
-          bsStyle="danger"
-          className="btn-block"
-          onClick={ handleRemoveDocument.bind(this, document._id) }>
-          Remove
-        </Button>
-      </Col>
-    </Row>
-  </ListGroupItem>
-);
+  <InlineCss stylesheet={`
+    .Document {
+    font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+    }
+    @media print {
+    .Document {
+    display: block;
+    boder: 1px solid red;
+    padding: 20px;
+    }
+    .btn { display: none }
+    hr { display: none }
+    h3 {
+    font-size: 28px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+    }
+    p {
+    margin-top: 10px;
+    margin-bottom: 0pxl
+    font-size: 18px;
+    }
+    }
+  `}>
+    <ListGroupItem className="Document">
+      <Button data-id={ document._id } onClick={ handleDownloadDocument } bsStyle="success">Download</Button>
+      <Button data-id={ document._id } onClick={ handleRemoveDocument } bsStyle="danger">Remove</Button>
+      <hr/>
+      <h3>{ document.title }</h3>
+      <p>{ document.body }</p>
+    </ListGroupItem>
+  </InlineCss>
+)
